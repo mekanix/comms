7d5881871005a272cbc96e2f47363980ca385146
Goran Mekić <meka@tilda.center>
2020-03-28 14:52:51 +0100

    Update ldap and jabber certs

--- bin/cron.sh
+++ bin/cron.sh
@@ -3,4 +3,6 @@
 /usr/local/bin/cbsd jstart letsencrypt
 /usr/local/bin/cbsd jexec jname=letsencrypt /usr/local/bin/letsencrypt_update.sh
 /usr/local/bin/cbsd jstop letsencrypt
+/usr/local/bin/cbsd jexec jname=ldap /usr/local/bin/update_certs.sh
+/usr/local/bin/cbsd jexec jname=jabber /usr/local/bin/update_certs.sh
 /usr/local/bin/cbsd jexec jname=nginx /usr/sbin/service nginx reload
