5d1e1aea22cdef78520f26b6be1201f37e3a71fe
Goran Mekić <meka@tilda.center>
2020-03-28 15:26:36 +0100

    Update cert for all services

--- bin/cron.sh
+++ bin/cron.sh
@@ -3,6 +3,9 @@
 /usr/local/bin/cbsd jstart letsencrypt
 /usr/local/bin/cbsd jexec jname=letsencrypt /usr/local/bin/letsencrypt_update.sh
 /usr/local/bin/cbsd jstop letsencrypt
+
 /usr/local/bin/cbsd jexec jname=ldap /usr/local/bin/update_certs.sh
 /usr/local/bin/cbsd jexec jname=jabber /usr/local/bin/update_certs.sh
+/usr/local/bin/cbsd jexec jname=mail /usr/sbin/service postfix reload
+/usr/local/bin/cbsd jexec jname=mail /usr/sbin/service dovecot reload
 /usr/local/bin/cbsd jexec jname=nginx /usr/sbin/service nginx reload
